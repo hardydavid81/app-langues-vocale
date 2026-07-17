0a45837aa8c432c19a1890220eb628b31a400e21
hardydavid81 <hardydavid.81@gmail.com>
2026-07-18 01:09:38 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _scrollToBottom();
     try {
       final url = Uri.parse(
-        'https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent',
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent',
       );
       final response = await http.post(
         url,
