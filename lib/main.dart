d63b08333d56f8e6d72be4d33c4f78ddbc0ff4b5
hardydavid81 <hardydavid.81@gmail.com>
2026-07-17 23:50:26 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,51 @@ class ChatMessage {
   ChatMessage(this.text, this.isUser);
 }
 
+class EyePainter extends CustomPainter {
+  final double openness;
+  EyePainter(this.openness);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final eyeWidth = size.width;
+    final eyeHeightMax = size.height;
+    final eyeHeight = (eyeHeightMax * openness).clamp(6.0, eyeHeightMax);
+
+    final rect = Rect.fromCenter(
+      center: center,
+      width: eyeWidth,
+      height: eyeHeight,
+    );
+    final rrect = RRect.fromRectAndRadius(rect, Radius.circular(eyeHeight / 2));
+
+    canvas.drawRRect(rrect, Paint()..color = Colors.white);
+    canvas.drawRRect(
+      rrect,
+      Paint()
+        ..color = Colors.black87
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 3,
+    );
+
+    if (openness > 0.25) {
+      final irisRadius = size.height * 0.32 * openness;
+      canvas.drawCircle(center, irisRadius, Paint()..color = Colors.blue);
+      canvas.drawCircle(
+          center, irisRadius * 0.45, Paint()..color = Colors.black);
+      canvas.drawCircle(
+        Offset(center.dx - irisRadius * 0.2, center.dy - irisRadius * 0.2),
+        irisRadius * 0.15,
+        Paint()..color = Colors.white,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant EyePainter oldDelegate) =>
+      oldDelegate.openness != openness;
+}
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -34,7 +79,8 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   final stt.SpeechToText _speech = stt.SpeechToText();
   final FlutterTts _tts = FlutterTts();
   final ScrollController _scrollController = ScrollController();
@@ -42,6 +88,24 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _isThinking = false;
   String _userText = "";
   final List<ChatMessage> _messages = [];
+
+  late AnimationController _eyeController;
+
+  @override
+  void initState() {
+    super.initState();
+    _eyeController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 350),
+      value: 0.0,
+    );
+  }
+
+  @override
+  void dispose() {
+    _eyeController.dispose();
+    super.dispose();
+  }
 
   void _scrollToBottom() {
     Future.delayed(const Duration(milliseconds: 100), () {
@@ -118,6 +182,7 @@ class _HomeScreenState extends State<HomeScreen> {
           _isListening = true;
           _userText = "";
         });
+        _eyeController.forward();
         _speech.listen(
           onResult: (result) {
             setState(() {
@@ -130,6 +195,7 @@ class _HomeScreenState extends State<HomeScreen> {
       setState(() {
         _isListening = false;
       });
+      _eyeController.reverse();
       await _speech.stop();
       if (_userText.isNotEmpty) {
         _askGemini(_userText);
@@ -171,7 +237,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: _messages.isEmpty
                 ? const Center(
                     child: Text(
-                      "Appuyez sur le micro et parlez",
+                      "Appuyez sur l'oeil et parlez",
                       style: TextStyle(fontSize: 18, color: Colors.grey),
                       textAlign: TextAlign.center,
                     ),
@@ -191,16 +257,28 @@ class _HomeScreenState extends State<HomeScreen> {
               child: CircularProgressIndicator(),
             ),
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(24.0),
             child: GestureDetector(
               onTap: _listen,
-              child: CircleAvatar(
-                radius: 36,
-                backgroundColor: _isListening ? Colors.red : Colors.blue,
-                child: const Icon(
-                  Icons.mic,
-                  size: 44,
-                  color: Colors.white,
+              child: Container(
+                width: 110,
+                height: 110,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _isListening
+                      ? Colors.red.shade100
+                      : Colors.blueGrey.shade100,
+                ),
+                child: Center(
+                  child: AnimatedBuilder(
+                    animation: _eyeController,
+                    builder: (context, child) {
+                      return CustomPaint(
+                        size: const Size(80, 50),
+                        painter: EyePainter(_eyeController.value),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
