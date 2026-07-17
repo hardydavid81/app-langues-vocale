6b0dbbff372e387943b7d63e625148c900e26267
hardydavid81 <hardydavid.81@gmail.com>
2026-07-17 17:25:47 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,76 @@
 import 'package:flutter/material.dart';
-import './ui/FlutterApp.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: "Sample App",
-    home: new FlutterApp(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Langues Vocale',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const ConversationScreen(),
+    );
+  }
+}
+
+class ConversationScreen extends StatefulWidget {
+  const ConversationScreen({super.key});
+
+  @override
+  State<ConversationScreen> createState() => _ConversationScreenState();
+}
+
+class _ConversationScreenState extends State<ConversationScreen> {
+  String _status = "Appuyez sur le micro pour parler";
+  bool _isListening = false;
+
+  void _toggleListening() {
+    setState(() {
+      _isListening = !_isListening;
+      _status = _isListening
+          ? "J'écoute..."
+          : "Appuyez sur le micro pour parler";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Apprentissage des langues'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _status,
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            GestureDetector(
+              onTap: _toggleListening,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: _isListening ? Colors.red : Colors.blue,
+                child: const Icon(
+                  Icons.mic,
+                  size: 50,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
